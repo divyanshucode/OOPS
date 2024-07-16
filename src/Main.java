c286288ea954ffb93aef288e1a877180287484cc
DIVYANSHU KANSWAL <kanswaldivyanshu@gmail.com>
2024-07-16 12:34:57 +0530

    Baiscs

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+      int[] rno = new int[5];
+      String[] name = new String[5];
+        }
+
+}
+
+class  Student {
+    int[] rno = new int[5];
+    String[] name = new String[5];
+}
